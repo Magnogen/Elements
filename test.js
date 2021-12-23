8180904caa3590c9e023b20db65e8f11f9ceeca2
Magnogen <hydronesium@gmail.com>
2021-12-23 14:09:11 +0000

    Fix Colour installation and add more Limits (#3)
    * Fix Colour installation and add more Limits
    
    * renamed `Math` to `Limit`, keeping more inline with its functionality
    * fixed the installation process using the [nullish assignment](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_nullish_assignment)
      (very pog if you ask me)
    
    * more tests and things

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 Elements.install(this)
 
 let teal = new Colour('cmy', 255, 75, 100)
-teal.hsl = [undefined, 0.5, 0.5]
+teal.s = 0.5
+teal.l = 0.5
 console.log('teal', teal.hsl)
 // a nice looking teal colour tbh
 document.body.style.backgroundColor = `rgba(${teal.r}, ${teal.g}, ${teal.b}, ${teal.a})`
@@ -11,3 +12,9 @@ peach.r = 255
 console.log('peach', peach.hsl)
 // a cute peach colour lmao
 document.body.style.backgroundColor = `rgba(${peach.r}, ${peach.g}, ${peach.b}, ${peach.a})`
+
+console.log(Limit.sigmoid(-Infinity, 0, 1)) // 0
+console.log(Limit.sigmoid(0, 0, 1)) // 0.1192...
+console.log(Limit.sigmoid(0.5, 0, 1)) // 0.5
+console.log(Limit.sigmoid(1, 0, 1)) //0.8807...
+console.log(Limit.sigmoid(Infinity, 0, 1)) // 1
