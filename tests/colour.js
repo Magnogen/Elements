c57f06f6d8cd82a6ad94230f9d1c5ce792a6a758
Magnogen <hydronesium@gmail.com>
2022-07-11 12:20:02 +0100

    update colour test

--- tests/colour.js
+++ tests/colour.js
@@ -1,15 +1,16 @@
-Elements.install(this)
+const { Colour } = Elements
 
 let teal = new Colour('cmy', 255, 75, 100)
 teal.s = 0.5
 teal.l = 0.5
 console.log('teal', teal.hsl)
 // a nice looking teal colour tbh
-document.body.style.backgroundColor = `rgba(${teal.r}, ${teal.g}, ${teal.b}, ${teal.a})`
+document.body.style.backgroundColor = teal.toHex()
 
 let peach = new Colour(teal)
 peach.r = 255
-console.log('peach', peach.hsl)
+console.log('peach', peach.rgb)
 // a cute peach colour lmao
-document.body.style.backgroundColor = `rgba(${peach.r}, ${peach.g}, ${peach.b}, ${peach.a})`
+document.body.style.backgroundColor = peach.toRGB()
+
 
