1be873bd2b9ecbbd45ce54249cf39b6d6a41c4ca
Magnogen <hydronesium@gmail.com>
2022-09-22 12:28:17 +0100

    test update feat. starting a thread later

--- tests/thread.js
+++ tests/thread.js
@@ -1,19 +1,15 @@
 const $ = query => document.querySelector(query)
 const { Thread, Colour } = Elements
 
-new Thread(async function* () {
+let vignette = new Thread(async function* () {
   let c = $('canvas')
   let ctx = c.getContext('2d')
-  c.width = c.height = 512
   
   let { width, height } = c
   
-  ctx.fillStyle = '#fff'
-  ctx.fillRect(0, 0, width, height)
-  
   const pow = (func, amount) => {
     let result = 1
-    for (let i = 0; i < amount; i++) r *= func()
+    for (let i = 0; i < amount; i++) result *= func()
     return result
   }
   
@@ -43,5 +39,36 @@ new Thread(async function* () {
   }
   yield 'done'
   
-}).start()
+})
 
+new Thread(function* () {
+    
+  let canvas = document.querySelector('canvas')
+  canvas.width = canvas.height = 1024
+  let ctx = canvas.getContext('2d')
+  let { width, height } = canvas
+
+  let pixels = ctx.getImageData(0, 0, width, height)
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      const i = 4 * (x + width*y)
+      pixels.data[i + 0] = Math.floor(Math.random() * 255)
+      pixels.data[i + 1] = Math.floor(Math.random() * 255)
+      pixels.data[i + 2] = Math.floor(Math.random() * 255)
+      pixels.data[i + 3] = 255
+      if ((yield) == Thread.waited) {
+        ctx.putImageData(pixels, 0, 0);
+      }
+    }
+  }
+  ctx.putImageData(pixels, 0, 0);
+
+  vignette.start()
+
+}).start()
+  
+  
+  
+  
+  
+  
